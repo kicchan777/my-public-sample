9f9fe4470f422bdf87d7330150ee940473211acb
kicchan777 <67027714+kicchan777@users.noreply.github.com>
2024-06-09 16:41:45 +0900

    Playwrightのe2eテストを追加 (#1)

---
+++ tests/login-ichiro.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://hotel.testplanisphere.dev/ja/');
+
+  // login
+  await page.getByRole('button', { name: 'ログイン' }).click();
+  await page.getByLabel('メールアドレス').fill('ichiro@example.com');
+  await page.getByLabel('パスワード').fill('password');
+  await page.locator('#login-button').click();
+ 
+  // expect
+  await expect(page.locator('#email')).toContainText('ichiro@example.com');
+  await expect(page.locator('#rank')).toContainText('プレミアム会員');
+
+  // logout
+  await page.getByRole('button', { name: 'ログアウト' }).click();
+  await expect(page.getByRole('button')).toContainText('ログイン');
+});
